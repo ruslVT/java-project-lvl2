f12de111641c5a414b6fb1de1b5516e8a20921c8
RuslT <ruslf2p@yandex.ru>
2022-07-17 17:33:18 +0300

    modified files

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -13,10 +13,14 @@ public class Differ {
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> map1 = mapper.readValue(new File(filePath1), new TypeReference<>() { });
         Map<String, Object> map2 = mapper.readValue(new File(filePath2), new TypeReference<>() { });
-        StringBuilder result = new StringBuilder("{\n");
-
         Set<String> keySet = new TreeSet<>(map1.keySet());
         keySet.addAll(map2.keySet());
+
+        return diff(map1, map2, keySet);
+    }
+
+    public static String diff(Map<String, Object> map1, Map<String, Object> map2, Set<String> keySet) {
+        StringBuilder result = new StringBuilder("{\n");
 
         for (String key : keySet) {
             if (!map1.containsKey(key) && map2.containsKey(key)) {
