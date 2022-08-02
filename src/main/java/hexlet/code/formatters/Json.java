d5fc6e812eec6144a60b25071059c8929d4c5fca
RuslT <ruslf2p@yandex.ru>
2022-08-02 23:24:14 +0300

    modified Json.java, expectedJson.txt

--- src/main/java/hexlet/code/formatters/Json.java
+++ src/main/java/hexlet/code/formatters/Json.java
@@ -2,19 +2,13 @@ package hexlet.code.formatters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Json {
 
     public static String format(List<Map<String, List<Object>>> diffList) throws Exception {
-
-        return new ObjectMapper().writeValueAsString(diffList
-                .stream()
-                .filter(map -> !map.containsKey("removed"))
-                .flatMap(map -> map.values().stream())
-                .collect(LinkedHashMap::new, (m, k) -> m.put(k.get(0), k.get(1)), LinkedHashMap::putAll));
+        return new ObjectMapper().writeValueAsString(diffList);
     }
 
 }
